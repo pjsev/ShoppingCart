3cac8e807f71dbb66410622fa10abe133d8dbe9f
pjsev <43801918+pjsev@users.noreply.github.com>
2019-06-20 18:02:57 +0800

    Add files via upload

---
+++ item_data.sql
@@ -0,0 +1,40 @@
+INSERT INTO `shopping_app`.`item`
+(`id`,
+`description`,
+`name`,
+`price`)
+VALUES
+(1,
+'apple sample','apple',10.50);
+INSERT INTO `shopping_app`.`item`
+(`id`,
+`description`,
+`name`,
+`price`)
+VALUES
+(2,
+'mango sample','mango',6.50);
+INSERT INTO `shopping_app`.`item`
+(`id`,
+`description`,
+`name`,
+`price`)
+VALUES
+(3,
+'banana sample','banana',4.00);
+INSERT INTO `shopping_app`.`item`
+(`id`,
+`description`,
+`name`,
+`price`)
+VALUES
+(4,
+'pineapple sample','pineapple',6.25);
+INSERT INTO `shopping_app`.`item`
+(`id`,
+`description`,
+`name`,
+`price`)
+VALUES
+(5,
+'melon sample','melon',8.55);
